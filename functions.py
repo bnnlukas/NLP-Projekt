afb4937d86ff0bc5283a1b5dfc7236d2e26138e5
Jasmina11 <77899903+Jasmina11@users.noreply.github.com>
2023-01-19 15:35:56 +0100

    Added and cleaned answers

--- functions.py
+++ functions.py
@@ -36,25 +36,25 @@ def return_answer(input, intent):
             year = [token.lemma_ for token in txt if token.ent_type_ == "DATE"]
             df = pd.read_csv('./data/FIFA - World Cup Summary.csv')
             host = df['HOST'].loc[df['YEAR'] == int(year[0])]
-            output = f'The host of the FIFA world cup in {year[0]} was {host.item()}'
+            output = f'The host of the FIFA World Cup in {year[0]} was {host.item()}.'
 
         elif intent == 'yearMatches':
             year = [token.lemma_ for token in txt if token.ent_type_ == "DATE"]
             df = pd.read_csv('./data/FIFA - World Cup Summary.csv')
             matches = df['MATCHES PLAYED'].loc[df['YEAR'] == int(year[0])]
-            output = f'In {year[0]} in total {matches.item()} matches were played'
+            output = f'In {year[0]} a total of {matches.item()} matches were played.'
 
         elif intent == 'year(avg)Goals':
             year = [token.lemma_ for token in txt if token.ent_type_ == "DATE"]
             df = pd.read_csv('./data/FIFA - World Cup Summary.csv')
             avgGoals = df['AVG GOALS PER GAME'].loc[df['YEAR'] == int(year[0])]
-            output = f'In {year[0]} in average {avgGoals.item()} goals where scored per game'
+            output = f'In {year[0]} on average {avgGoals.item()} goals were scored per game.'
 
         elif intent == 'yearGoals':
             year = [token.lemma_ for token in txt if token.ent_type_ == "DATE"]
             df = pd.read_csv('./data/FIFA - World Cup Summary.csv')
             avgGoals = df['GOALS SCORED'].loc[df['YEAR'] == int(year[0])]
-            output = f'In {year[0]} in total {avgGoals.item()} goals where scored'
+            output = f'In {year[0]} in total {avgGoals.item()} goals were scored.'
 
         elif intent == 'PlacementTeam':
             ord_dict = txt._.numerize(labels = 'ORDINAL')
@@ -66,27 +66,31 @@ def return_answer(input, intent):
             pos_str = [token.lemma_ for token in txt if token.ent_type_ == "ORDINAL"]
             pos_int = int(re.search(r'\d+', pos_str[0]).group())
             pos_team = df['Team'].loc[df['Position'] == pos_int]
-            output = f'At the {year[0]} World Cup, the {pos_str[0]} place was taken by {pos_team.item()}'
+            output = f'At the World Cup in {year[0]}, the {pos_str[0]} place was taken by {pos_team.item()}.'
         
         elif intent == 'TeamPlacement':
             year = [token.lemma_ for token in txt if token.ent_type_ == "DATE"]
             try:
                 df = pd.read_csv(f'./data/FIFA - {int(year[0])}.csv')
             except:
-                output = 'In this year no world cup was played'
+                output = 'In this year no World Cup was played.'
                 return output
             nation = [token.lemma_ for token in txt if token.ent_type_ == "GPE"]
             team_pos = df['Position'].loc[df['Team'] == nation[0].capitalize()]
-            output = f'At the {year[0]} World Cup, {nation[0]} finished the {team_pos.item()}th place'
+            output = f'In the {year[0]} World Cup, {nation[0]} finished in the {team_pos.item()}th place'
+
         elif intent == 'greeting':
             output = f"""Hi, welcome to the FIFA World Cup Chatbot! I was created in the Natural Language 
             Processing lecture of WWI20DSA by Pascal, Lukas, Jasmina and Aymane. I will answer your 
             questions regarding all the FIFA World Cups as best as I can. Do you have a question already?
             I am happy to help you out :)"""
+
         elif intent == 'thankYou':
-            output = f'You are welcome! Do you have any additional questions? To get to our easter egg for some fun, you can ask me who the best player of all time is! '
+            output = f'You are welcome! Do you have any additional questions? To get to our easter egg for some fun, you can ask me who the best player of all time is!'
+
         elif intent == 'bye':
             output = f'Good bye, it was nice chatting with you. See you again soon!'
+
         return output
     except:
-        return 'Please type in your question again'
+        return 'I am sorry, but I cannot answer your question. Could you rephrase it for me please?'
