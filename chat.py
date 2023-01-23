b6308ddf4ddc6f7daa555175763858af808cd0a9
bnnlukas <bonn_lukas@gmx.de>
2023-01-23 16:10:05 +0100

    Descriptions added

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ from functions import return_answer
 
 bot_name = "Bot"
 
+# Definieren der Funktion, welche für einen gegebenen Eingabetext eine Antwort bestimmt und zurückgibt
 def get_response(input):
     intent = get_intent(input)
     response = return_answer(input, intent)
--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
+# allgemeine Imports
 import pandas as pd
 import re
 
-# spacy imports
+# spacy Imports
 import spacy
 from spacy.lang.en.examples import sentences 
 from numerizer import numerize
 
+# Definieren des deplace_dicts zum späteren Ersetzen von Satzbausteinen
 replace_dict = {
     'last world championship': 'World Cup in 2022',
     'last world-championship': 'World Cup in 2022',
@@ -20,6 +22,7 @@ replace_dict = {
 def return_answer(input, intent):
     nlp = spacy.load("en_core_web_sm")
     input = input.lower()
+    # Ersetzen von Satzbausteinen, um diese für die NER-Analyse einsetzbar zu machen
     for key in replace_dict.keys():
         if key in input:
             input = input.replace(str(key), replace_dict[key])
@@ -27,6 +30,7 @@ def return_answer(input, intent):
             pass
     txt = nlp(input)
     try:
+        # Festlegen der Verarbeitungsschritte für verschiedene erkannte Intents
         if intent == 'YearHost':
             year = [token.lemma_ for token in txt if token.ent_type_ == "DATE"]
             df = pd.read_csv('./data/FIFA - World Cup Summary.csv')
@@ -64,6 +68,7 @@ def return_answer(input, intent):
                 output = f'In {year[0]} in total {avgGoals.item()} goals were scored.'
 
         elif intent == 'PlacementTeam':
+            # Ersetzen von Ordinalwerten in englischer Sprache in Integer (first -> 1st), um die NER zu ermöglichen
             ord_dict = txt._.numerize(labels = 'ORDINAL')
             for key in ord_dict.keys():
                 txt_modified = input.replace(str(key), ord_dict[key])
@@ -75,6 +80,7 @@ def return_answer(input, intent):
                 output = 'In this year no World Cup was played. Please type in a correct year'
                 return output
             pos_str = [token.lemma_ for token in txt if token.ent_type_ == "ORDINAL"]
+            # Entfernen von Buchstaben in Ordinalwerten um Suche in dem Dataframe zu ermöglichen (1st -> 1)
             pos_int = int(re.search(r'\d+', pos_str[0]).group())
             pos_team = df['Team'].loc[df['Position'] == pos_int]
             output = f'At the World Cup in {year[0]}, the {pos_str[0]} place was taken by {pos_team.item()}.'
@@ -96,6 +102,7 @@ def return_answer(input, intent):
             except:
                 output = 'In this year no World Cup was played. Please type in a correct year'
                 return output
+            # Festlegen des Umgangs mit Nationen, welche aus mehreren Wörtern bestehen (Saudi Arabia, South Africa, etc.)
             nation = [token.lemma_ for token in txt if token.ent_type_ == "GPE"]
             if len(nation) > 1:
                 nation = [" ".join(nation)]
@@ -103,7 +110,7 @@ def return_answer(input, intent):
                 pass
             team_pos = df['Position'].loc[df['Team'] == nation[0].title()]
             output = f'In the {year[0]} World Cup, {nation[0].title()} finished in the {team_pos.item()}th place'
-
+        # Antwort für erkannte allgemeine Intents
         elif intent == 'greeting':
             output = f"""Hi, welcome to the FIFA World Cup Chatbot! I was created in the Natural Language 
             Processing lecture of WWI20DSA by Pascal, Lukas, Jasmina and Aymane. I will answer your 
